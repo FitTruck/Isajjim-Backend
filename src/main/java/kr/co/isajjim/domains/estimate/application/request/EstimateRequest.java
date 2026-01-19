d112da19a0c3be1fc54bcd23456e7fa683a8ce3c
hyotatoFrappuccino <hjkiln@naver.com>
2026-01-19 16:15:25 +0900

    ♻️ refactor: imageUrls 검증 추가

--- src/main/java/kr/co/isajjim/domains/estimate/application/request/EstimateRequest.java
+++ src/main/java/kr/co/isajjim/domains/estimate/application/request/EstimateRequest.java
@@ -1,8 +1,14 @@
 package kr.co.isajjim.domains.estimate.application.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.URL;
+
 import java.util.List;
 
 public record EstimateRequest(
-        List<String> imageUrls
+
+        @NotEmpty
+        List<@NotBlank @URL String> imageUrls
 ) {
 }
