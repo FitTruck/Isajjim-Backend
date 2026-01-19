7af30b75523afe94fa22c44a4d2bfcf00b7192a8
hyotatoFrappuccino <hjkiln@naver.com>
2026-01-19 17:16:51 +0900

    📝 docs: Swagger 서버 URL 추가(환경별 대응)

--- src/main/java/kr/co/isajjim/global/config/swagger/SwaggerConfig.java
+++ src/main/java/kr/co/isajjim/global/config/swagger/SwaggerConfig.java
@@ -3,8 +3,10 @@ package kr.co.isajjim.global.config.swagger;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.HandlerMethod;
@@ -13,12 +15,15 @@ import org.springframework.web.method.HandlerMethod;
 @RequiredArgsConstructor
 public class SwaggerConfig {
 
+    @Value("${swagger.server-url}")
+    private String serverUrl;
     private final ApiSuccessResponseHandler apiSuccessResponseHandler;
     private final ApiErrorResponseHandler apiErrorResponseHandler;
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url(serverUrl))
                 .info(new Info()
                         .title("Isajjim API")
                         .description("이삿찜 백엔드 API 문서")
