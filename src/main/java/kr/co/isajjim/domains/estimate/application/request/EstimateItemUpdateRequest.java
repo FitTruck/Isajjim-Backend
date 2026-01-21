b9c7c2304f4031f12eaf5e4f0cc59aed547af5a6
hyotatoFrappuccino <hjkiln@naver.com>
2026-01-21 17:06:07 +0900

    ♻️ refactor: 가구 수량 Validation 0~100으로 설정

--- src/main/java/kr/co/isajjim/domains/estimate/application/request/EstimateItemUpdateRequest.java
+++ src/main/java/kr/co/isajjim/domains/estimate/application/request/EstimateItemUpdateRequest.java
@@ -1,6 +1,8 @@
 package kr.co.isajjim.domains.estimate.application.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record EstimateItemUpdateRequest(
@@ -16,7 +18,8 @@ public record EstimateItemUpdateRequest(
                 description = "가구 수량",
                 example = "1"
         )
-        @NotNull
+        @Min(0)
+        @Max(100)
         Integer quantity
 ) {
 }
