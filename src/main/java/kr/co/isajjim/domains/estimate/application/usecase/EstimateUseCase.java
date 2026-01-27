68afd88941044130e2ee268e57a299470f26ad14
hyotatoFrappuccino <hjkiln@naver.com>
2026-01-27 10:29:23 +0900

    ♻️ refactor: Mock데이터 수정

--- src/main/java/kr/co/isajjim/domains/estimate/application/usecase/EstimateUseCase.java
+++ src/main/java/kr/co/isajjim/domains/estimate/application/usecase/EstimateUseCase.java
@@ -103,8 +103,7 @@ public class EstimateUseCase {
                         new AIAnalysisResponse.FurnitureInfo(FurnitureLabel.BED, FurnitureType.BUNK_BED, 30.5, 20.0, 15.2, 1.0),
                         new AIAnalysisResponse.FurnitureInfo(FurnitureLabel.BED, FurnitureType.DOUBLE_BED, 30.5, 20.0, 15.2, 1.0),
                         new AIAnalysisResponse.FurnitureInfo(FurnitureLabel.PIANO, FurnitureType.GRAND_PIANO, 210.0, 90.0, 85.0, 1.0),
-                        new AIAnalysisResponse.FurnitureInfo(FurnitureLabel.PIANO, FurnitureType.DIGITAL_PIANO, 210.0, 90.0, 85.0, 1.0),
-                        new AIAnalysisResponse.FurnitureInfo(FurnitureLabel.BICYCLE, null, 210.0, 90.0, 85.0, 1.0)
+                        new AIAnalysisResponse.FurnitureInfo(FurnitureLabel.PIANO, FurnitureType.DIGITAL_PIANO, 210.0, 90.0, 85.0, 1.0)
                 )
         )).toList();
 
