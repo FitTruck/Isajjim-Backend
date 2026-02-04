94bd7802109e2ec52de758a16c6868ce8612ecf1
hyotatoFrappuccino <hjkiln@naver.com>
2026-02-04 10:01:39 +0900

    ♻️ refactor: 가구 엔티티 및 응답에 이미지 내 좌표 추가

--- src/main/java/kr/co/isajjim/domains/furniture/persistence/entity/Furniture.java
+++ src/main/java/kr/co/isajjim/domains/furniture/persistence/entity/Furniture.java
@@ -45,6 +45,10 @@ public class Furniture extends BaseEntity {
 
     private Integer quantity;
 
+    private Double centerX;
+
+    private Double centerY;
+
     @Builder
     private Furniture(
         FurnitureLabel label,
@@ -54,7 +58,9 @@ public class Furniture extends BaseEntity {
         Double depth,
         Double volume,
         String plyUrl,
-        Integer quantity
+        Integer quantity,
+        Double centerX,
+        Double centerY
     ) {
         this.label = label;
         this.type = type;
@@ -64,6 +70,8 @@ public class Furniture extends BaseEntity {
         this.volume = volume;
         this.plyUrl = plyUrl;
         this.quantity = quantity;
+        this.centerX = centerX;
+        this.centerY = centerY;
     }
 
     public void setImage(Image image) {
