a8cfeced1ceabe0b53b5c87eea580ccb09b9f1e9
hyotatoFrappuccino <hjkiln@naver.com>
2026-03-27 19:32:08 +0900

    ♻️ refactor: @Transactional 스프링 패키지로 변경

--- src/main/java/kr/co/isajjim/global/security/service/CustomOAuth2UserService.java
+++ src/main/java/kr/co/isajjim/global/security/service/CustomOAuth2UserService.java
@@ -1,7 +1,6 @@
 package kr.co.isajjim.global.security.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.transaction.Transactional;
 import kr.co.isajjim.domains.user.persistence.entity.UserEntity;
 import kr.co.isajjim.domains.user.persistence.repository.UserRepository;
 import kr.co.isajjim.global.security.auth.CustomUserDetails;
@@ -15,6 +14,7 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 
