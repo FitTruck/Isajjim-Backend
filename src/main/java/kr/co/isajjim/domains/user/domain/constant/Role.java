9753b3d56a78e811117d710e810b8fedd34e728e
hyotatoFrappuccino <hjkiln@naver.com>
2026-04-24 16:07:59 +0900

    ✨ feat: User - 파트너 Role 추가

--- src/main/java/kr/co/isajjim/domains/user/domain/constant/Role.java
+++ src/main/java/kr/co/isajjim/domains/user/domain/constant/Role.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Role {
 
-    // 필요에 따라 추가.
     USER("ROLE_USER", "사용자"),
+    PARTNER("ROLE_PARTNER", "파트너"),
     ADMIN("ROLE_ADMIN", "관리자"),
     ;
 
