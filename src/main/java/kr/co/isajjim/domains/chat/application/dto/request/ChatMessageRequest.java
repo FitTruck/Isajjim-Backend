76d2ffb6b8d5aed24bf01deb3d58a9a86820bc63
hyotatoFrappuccino <hjkiln0@gmail.com>
2026-05-08 22:50:17 +0900

    🐛 fix: 채팅 메시지 content 최대 길이 검증 추가

--- src/main/java/kr/co/isajjim/domains/chat/application/dto/request/ChatMessageRequest.java
+++ src/main/java/kr/co/isajjim/domains/chat/application/dto/request/ChatMessageRequest.java
@@ -2,10 +2,11 @@ package kr.co.isajjim.domains.chat.application.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import kr.co.isajjim.domains.chat.domain.constant.MessageType;
 
 public record ChatMessageRequest(
-        @NotBlank String content,
+        @NotBlank @Size(max = 2000) String content,
         @NotNull MessageType type
 ) {
 }
